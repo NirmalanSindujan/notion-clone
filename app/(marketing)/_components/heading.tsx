3fa04cf0e5b2e62158a52393c76b94fe802523ec
sindujan <sindujan96@gmail.com>
2023-11-06 00:12:38 +0530

    markeign pages added

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export const Heading =()=>{
+ return(
+    <div className="max-w-3xl space-y-4">
+        <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold"> Ypur Ideas,Documents, & Plans. Unified </h1>
+    </div>
+ )
+}
+
