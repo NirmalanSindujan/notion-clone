d540735592dd4b908a8001326e38f3ce1fe8c3aa
sindujan <sindujan96@gmail.com>
2023-11-06 23:55:28 +0530

    Footer hero and logo page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,5 +1,7 @@
 import { Button } from '@/components/ui/button'
 import { Heading } from './_components/heading';
+import { Heroes } from './_components/heroes';
+import { Footer } from './_components/footer';
 
 
 const MarketingPage = () =>{
@@ -9,7 +11,10 @@ const MarketingPage = () =>{
     md:justify-start text-center gap-y-8 flex-1 px-6 pc-10'>
       <Heading />
 
+      <Heroes/>
     </div>
+
+    <Footer/>
    </div>
   );
 }
