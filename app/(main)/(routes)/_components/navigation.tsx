8d7eb11e44a9b0b61b77f88ddab6ae492c9d5640
sindujan <sindujan96@gmail.com>
2023-11-08 23:37:24 +0530

    handle mouse down

--- app/(main)/(routes)/_components/navigation.tsx
+++ app/(main)/(routes)/_components/navigation.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { ChevronLeft, ChevronsLeft } from "lucide-react";
+import { ChevronLeft, ChevronsLeft, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
@@ -19,6 +19,33 @@ const Navigation = () => {
     const [isCollapsed,setIsCollapsed] = useState(isMobile)
 
 
+    const handleMouseDown =(event : React.MouseEvent<HTMLDivElement,MouseEvent>)=>{
+        event.preventDefault();
+        event.stopPropagation();
+
+        isResizingRef.current = true;
+        document.addEventListener("mousemove",handleMouseMove);
+        document.addEventListener("mouseup",handleMouseUp);
+
+    }
+
+    const handleMouseMove = (e:MouseEvent)=>{
+        if(!isResizingRef.current) return
+        
+        let newWidth = e.clientX
+
+        if(newWidth< 240){
+            newWidth = 240
+         }
+         if(newWidth > 480){
+            newWidth = 480
+         }
+
+         if(sidebarRef.current && navbarRef.current){
+            sidebarRef.current.style.width = `${newWidth}px`
+            navbarRef.current.style.setProperty("left",`${newWidth}px` )
+        }
+
 
 
   return (
@@ -32,7 +59,10 @@ const Navigation = () => {
         )}>
         <div
           role="button"
-          className="h-6w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition"
+          className={cn(
+            "h-6w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
+            isMobile && "opacity-100"           
+            )}
         >
           <ChevronsLeft className="h-6 w-6" />{" "}
         </div>
@@ -44,8 +74,23 @@ const Navigation = () => {
         <div className="mt-4">
           <p>Docuemnts</p>
         </div>
-        <div className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
+        <div 
+        onMouseDown={()=>{handleMouseDown()}}
+        onClick={()=>{}}
+        className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
       </aside>
+
+      <div
+      ref={navbarRef}
+      className={cn("absolute top-0 z-[99999] left-60 w-[calc(100% -240px)]",
+      isResetting && "transition ease-in-out duration-300",
+      isMobile && "left-0 w-full"
+      )}
+      >
+        <nav  className="bg-transparent px-3 py-2 w-full">
+            {isCollapsed && <MenuIcon role="button" className="h-6 w-6 text-muted-foreground" />}
+        </nav>
+      </div>
     </>
   );
 };
