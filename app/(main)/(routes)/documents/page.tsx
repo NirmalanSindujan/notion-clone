5d543492533504bf0a5229cfc3fe3204d624246f
sindujan <sindujan96@gmail.com>
2023-11-11 16:21:31 +0530

    add documents

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -4,10 +4,29 @@ import Image from "next/image";
 import { useUser } from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
 import { PlusCircle } from "lucide-react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import {toast} from "sonner"
 
 const Documents = () => {
 
     const {user} = useUser();
+    const create = useMutation(api.documents.create)
+
+    const onCreate = () =>{
+        const promise = create({
+            title : "Untitled"
+        });
+
+        toast.promise(promise,{
+            loading : "Creating A New Note",
+            success : "New Note Created",
+            error : "Failed to Create A nite"
+        })
+
+    }
+
+
     return (
     <div className="h-full flex flex-col items-center justify-center">
       <Image
@@ -29,7 +48,7 @@ const Documents = () => {
       Welcome to {user?.firstName}&apos;s Jotion
       </h2>
 
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircle className="h-4 w-4 mr-2"/>
         Create Note
       </Button>
