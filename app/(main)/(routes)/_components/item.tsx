5e97b3f48bf13bf733a4242cdb1436187c4284f7
sindujan <sindujan96@gmail.com>
2023-11-21 01:12:25 +0530

    side bar 3.52

--- app/(main)/(routes)/_components/item.tsx
+++ app/(main)/(routes)/_components/item.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import { DropdownMenu , DropdownMenuTrigger,DropdownMenuContent,DropdownMenuItem,DropdownMenuSeparator } from "@/components/ui/dropdown-menu";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
 import { useMutation } from "convex/react";
-import { ChevronDown, ChevronRight, LucideIcon, Plus } from "lucide-react";
+import { ChevronDown, ChevronRight, LucideIcon, MoreHorizontal, Plus } from "lucide-react";
 import { useRouter } from "next/navigation";
 import React from "react";
 import { toast } from "sonner";
@@ -102,6 +103,16 @@ export const Item = ({
       )}
       {!!id && (
         <div className="ml-auto flex items-center gap-x-2 pr-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger
+            onClick={(e)=>e.stopPropagation()}
+            asChild>
+              <div role="button"
+              className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600">
+                <MoreHorizontal className="h-4 w-4 text-muted-foreground"/>
+              </div>
+            </DropdownMenuTrigger>
+          </DropdownMenu>
           <div
             role="button"
             onClick={onCreate}
@@ -110,6 +121,8 @@ export const Item = ({
           >
             <Plus className="h-4 w-4 text-muted-foreground" />
           </div>
+
+
         </div>
       )}
     </div>
