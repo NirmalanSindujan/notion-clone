a6194abcc8b8527737b34a55413ae320187f1b7c
sindujan <sindujan96@gmail.com>
2023-11-20 23:52:56 +0530

    crating children side bar

--- app/(main)/(routes)/_components/item.tsx
+++ app/(main)/(routes)/_components/item.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
-import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
+import { useMutation } from "convex/react";
+import { ChevronDown, ChevronRight, LucideIcon, Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
 import React from "react";
+import { toast } from "sonner";
 
 interface ItemProps {
   id?: Id<"documents">;
@@ -13,7 +17,7 @@ interface ItemProps {
   expanded?: boolean;
   isSearch?: boolean;
   level?: number;
-  onExpand?: ()=> void
+  onExpand?: () => void;
   label: string;
   onClick: () => void;
   icon: LucideIcon;
@@ -31,68 +35,95 @@ export const Item = ({
   onExpand,
   icon: Icon,
 }: ItemProps) => {
+  const create = useMutation(api.documents.create);
+  const router = useRouter();
 
-  const handleExpand =(event : React.MouseEvent<HTMLDivElement,MouseEvent>)=>{
+  const handleExpand = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
     event.stopPropagation();
     onExpand?.();
-  }
+  };
 
-    const ChevronIcon = expanded ? ChevronDown : ChevronRight
+  const onCreate = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (!id) return;
+    event.stopPropagation();
+    const promise = create({
+      title: "Untitled Children",
+      parentDocument: id,
+    }).then((documentId) => {
+      if (!expanded) {
+        onExpand?.();
+      }
+      // router.push(`/document/${documentId}`)
+    });
 
+    toast.promise(promise, {
+      loading: "Creting",
+      success: "New Note Crearted",
+      error: "Failed to create",
+    });
+  };
+
+  const ChevronIcon = expanded ? ChevronDown : ChevronRight;
 
   return (
     <div
       onClick={onClick}
       role="button"
-      style={{ paddingLeft: level? `${(level* 12)+12}px` : "12px" }}
+      style={{ paddingLeft: level ? `${level * 12 + 12}px` : "12px" }}
       className={cn(
         "group min-h-[27px] text-sm py-1 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium",
         active && "bg-primary/5 text-primary"
-        
-        
-        )}
+      )}
     >
-        {!!id && (
-            <div role="button"
-            className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
-            onClick={handleExpand}
-            >
-                <ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground/50"/>
-                </div>
-        )}
-        {documentIcon ? (
-            <div className="shrink-0 mr-2 text-[18px]"> 
-                {documentIcon}
-            </div>
-        ) :(
-            <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
-
-        )}
+      {!!id && (
+        <div
+          role="button"
+          className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
+          onClick={handleExpand}
+        >
+          <ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground/50" />
+        </div>
+      )}
+      {documentIcon ? (
+        <div className="shrink-0 mr-2 text-[18px]">{documentIcon}</div>
+      ) : (
+        <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+      )}
       <span className="truncate">{label}</span>
-      {
-        isSearch && (
-            <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px]
-                 font-medium text-muted-foreground opacity-100">
-                <span className="text-xs">
-                    CMD
-                </span>K
-            </kbd>
-        )
-      }
+      {isSearch && (
+        <kbd
+          className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px]
+                 font-medium text-muted-foreground opacity-100"
+        >
+          <span className="text-xs">CMD</span>K
+        </kbd>
+      )}
+      {!!id && (
+        <div className="ml-auto flex items-center gap-x-2 pr-2">
+          <div
+            role="button"
+            onClick={onCreate}
+            className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300
+      dark:hover:bg-neutral-600"
+          >
+            <Plus className="h-4 w-4 text-muted-foreground" />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
-
-Item.Skeleton = function ItemSkeleton({level} : {level ?: number}){
-    return(
-        <div style={{paddingLeft : level ? `${(level * 12) + 12}px` : "12px"}} 
-            className="flex gap-x-2 py-[3px]"
-        >
-
-            <Skeleton  className="h-4 w-4" />
-            <Skeleton  className="h-4 w-[30%]" />
-
-        </div>
-    )
-}
+Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
+  return (
+    <div
+      style={{ paddingLeft: level ? `${level * 12 + 12}px` : "12px" }}
+      className="flex gap-x-2 py-[3px]"
+    >
+      <Skeleton className="h-4 w-4" />
+      <Skeleton className="h-4 w-[30%]" />
+    </div>
+  );
+};
