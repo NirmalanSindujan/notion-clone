47f4b34418ee02ba844db83f649e662cae8d917d
sindujan <sindujan96@gmail.com>
2024-05-04 16:11:56 +0530

    archive pages

--- app/(main)/(routes)/_components/item.tsx
+++ app/(main)/(routes)/_components/item.tsx
@@ -5,8 +5,9 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
+import { useUser } from "@clerk/clerk-react";
 import { useMutation } from "convex/react";
-import { ChevronDown, ChevronRight, LucideIcon, MoreHorizontal, Plus } from "lucide-react";
+import { ChevronDown, ChevronRight, LucideIcon, MoreHorizontal, Plus, Trash } from "lucide-react";
 import { useRouter } from "next/navigation";
 import React from "react";
 import { toast } from "sonner";
@@ -36,8 +37,22 @@ export const Item = ({
   onExpand,
   icon: Icon,
 }: ItemProps) => {
+  const {user} = useUser();
   const create = useMutation(api.documents.create);
+  const archive = useMutation(api.documents.archive);
   const router = useRouter();
+
+
+  const onArchive=(event : React.MouseEvent<HTMLDivElement, MouseEvent>)=>{
+   event.stopPropagation()
+    if(!id) return;
+    const promise = archive({id});
+    toast.promise(promise,{
+      loading : "Moving to trash...",
+      success : "Note moved to trash",
+      error : "Failed to archive note"
+    })
+  };
 
   const handleExpand = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
@@ -112,6 +127,21 @@ export const Item = ({
                 <MoreHorizontal className="h-4 w-4 text-muted-foreground"/>
               </div>
             </DropdownMenuTrigger>
+            <DropdownMenuContent 
+            className="w-60"
+            align="start"
+            side="right"
+            forceMount
+            > 
+              <DropdownMenuItem onClick={onArchive}>
+                <Trash className="h-4 w-4 mr-2" />
+                Delete
+              </DropdownMenuItem>
+              <DropdownMenuSeparator/>
+              <div className="text-xs text-muted-foreground p-2">
+                Last Edited By : {user?.fullName}
+              </div>
+            </DropdownMenuContent>
           </DropdownMenu> 
           <div
             role="button"
