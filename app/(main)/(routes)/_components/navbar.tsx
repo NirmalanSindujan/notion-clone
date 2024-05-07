26c38637f84e25c4b4832d81945d61bcf3d68f48
sindujan <sindujan96@gmail.com>
2024-05-08 00:27:22 +0530

    Banner after delete

--- app/(main)/(routes)/_components/navbar.tsx
+++ app/(main)/(routes)/_components/navbar.tsx
@@ -6,6 +6,7 @@ import { useQuery } from "convex/react"
 import { MenuIcon } from "lucide-react"
 import { useParams } from "next/navigation"
 import { Title } from "./title"
+import { Banner } from "./banner"
 
 
 interface NavbarProps{
@@ -55,6 +56,10 @@ export const Navbar = ({
 
         </div>
        </nav>
+
+       {document.isArchived && (
+        <Banner documentId= {document._id} />
+       )}
        </>
     )
 }
