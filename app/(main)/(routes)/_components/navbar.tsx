52d0fb6e6b8054c523c67ed9aae3f5ba9c7a5db5
sindujan <sindujan96@gmail.com>
2024-05-08 23:58:47 +0530

    dcuent page

--- app/(main)/(routes)/_components/navbar.tsx
+++ app/(main)/(routes)/_components/navbar.tsx
@@ -3,10 +3,12 @@
 import { api } from "@/convex/_generated/api"
 import { Id } from "@/convex/_generated/dataModel"
 import { useQuery } from "convex/react"
-import { MenuIcon } from "lucide-react"
+import {   MenuIcon } from "lucide-react"
 import { useParams } from "next/navigation"
 import { Title } from "./title"
 import { Banner } from "./banner"
+import { Menu } from "./menu"
+
 
 
 interface NavbarProps{
@@ -27,8 +29,11 @@ export const Navbar = ({
 
     if(document === undefined){
         return(
-            <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center ">
+            <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center justify-center">
                 <Title.Skeleton />
+                <div className="flex items-center gap-x-2">
+                    <Menu.Skeleton />
+                </div>
             </nav>
         )
     }
@@ -53,6 +58,11 @@ export const Navbar = ({
 
         <div className="flex items-center justify-between w-full">
          <Title initialData= {document} />
+         <div className="flex items-center gap-x-2">
+
+            <Menu documentId={document._id}/>
+
+         </div>
 
         </div>
        </nav>
--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,4 +1,29 @@
-const DocumentIdPage = () =>{
+"use client"
+
+import { api } from "@/convex/_generated/api"
+import { Id } from "@/convex/_generated/dataModel"
+import { useQuery } from "convex/react"
+
+
+interface DocumentIdPageInterface {
+    params : {
+        documentId : Id<"documents">
+    }
+}
+
+const DocumentIdPage = ({
+    params
+} : DocumentIdPageInterface) =>{
+
+    const document = useQuery(api.documents.getById,{
+        documentId : params.documentId
+    })
+
+    if(document === undefined){
+        <div>
+            Loading...
+        </div>
+    }
     return(
         <div>
             Page
@@ -8,4 +33,3 @@ const DocumentIdPage = () =>{
 
 export default DocumentIdPage
 
-//5.00.46
