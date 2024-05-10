ab259837e78b581d7f641bb1bc73dc8af723ce46
sindujan <sindujan96@gmail.com>
2024-05-11 01:51:31 +0530

    heading added

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -4,13 +4,52 @@ import { Doc, Id } from "@/convex/_generated/dataModel";
 import { IconPicker } from "./ui/icon-picker";
 import { Button } from "@/components/ui/button";
 import { ImageIcon, Smile, X } from "lucide-react";
-
+import React, { ElementRef, useRef, useState } from "react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import TextareaAutoSize from "react-textarea-autosize";
 interface ToolbarProps {
   initialData: Doc<"documents">;
   preview?: boolean;
 }
 
 export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
+  const inputRef = useRef<ElementRef<"textarea">>(null);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const [value, setValue] = useState(initialData.title);
+
+  const update = useMutation(api.documents.update);
+
+  const enableInput = () => {
+    if (preview) return;
+
+    setIsEditing(true);
+    setTimeout(() => {
+      setValue(initialData.title);
+      inputRef.current?.focus();
+    }, 0);
+  };
+
+  const disbleInput = () => {
+    setIsEditing(false);
+  };
+
+  const onInput = (value: string) => {
+    setValue(value);
+    update({
+      id: initialData._id,
+      title: value || "Untitled",
+    });
+  };
+
+  const onKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      disbleInput();
+    }
+  };
+
   return (
     <div className="pl-[54px] group relative">
       {!!initialData.icon && !preview && (
@@ -54,9 +93,26 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
             size={"sm"}
           >
             <ImageIcon className="h-w w-4 mr-2" />
+            Add Cover
           </Button>
         )}
       </div>
+      {isEditing && !preview ? (
+        <TextareaAutoSize
+          ref={inputRef}
+          onBlur={disbleInput}
+          onKeyDown={onKeyDown}
+          value={value}
+          onChange={(e) => onInput(e.target.value)}
+          className="text-5xl bg-transparent font-bold break-words outline-none text-[#3f3f3f] dark:text-[#CFCFCF] resize-none"
+        />
+      ) : (
+        <div onClick={enableInput}
+        className="pb-[11.5px] text-5xl font-bold break-words outline-none  text-[#3f3f3f] dark:text-[#CFCFCF] "
+        >
+            {initialData.title}
+        </div>
+      )}
     </div>
   );
 };
