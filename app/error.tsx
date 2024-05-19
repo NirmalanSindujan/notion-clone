e9847b2a8dbaaec1d870a6cfadcea4c8dcbaa803
sindujan <sindujan96@gmail.com>
2024-05-20 01:09:42 +0530

    error handling

---
+++ app/error.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+import Link from "next/link"
+
+
+const Error = () => {
+    return ( 
+        <div className=" h-full flex flex-col items-center justify-center space-y-4">
+            <Image src={'/error.png'} 
+                height={"300"}
+                width={"300"}
+                alt="error"
+                
+            />
+            <h2 className="text-xl font-medium">
+                Something Went wrong
+            </h2>
+            <Button asChild>
+                <Link href={"/documents"}>
+                Go back
+                </Link>
+            </Button>
+        </div>
+     );
+}
+ 
+export default Error;
