19c31e40fd3fa01b29952dbd40f38bba12c45d56
sindujan <sindujan96@gmail.com>
2024-05-19 23:51:01 +0530

    block note chages added

--- components/ui/editor.tsx
+++ components/ui/editor.tsx
@@ -1,11 +1,32 @@
 "use client"
 
-import {BlockNoteEditor, PartialBlock} from "@blocknote/core"
+import {BlockNoteEditor, PartialBlock } from "@blocknote/core"
+import { useCreateBlockNote } from "@blocknote/react";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+import { useTheme } from "next-themes";
 
-export const Editor = () =>{
+interface EditorProps {
+    onChange : (value : string) => void;
+    initialContent ?: string;
+    editable?: boolean
+}
+
+
+export const Editor = ({onChange,initialContent,editable}:EditorProps) =>{
+
+    const {resolvedTheme} = useTheme()
+
+    const editor: BlockNoteEditor = useCreateBlockNote({
+        
+        initialContent : initialContent ? JSON.parse(initialContent) as PartialBlock[] : undefined,
+        // onEditorContentChange(ed)
+        }
+    )
+
     return(
         <div>
-            Editor
+           <BlockNoteView editor={editor} theme={resolvedTheme === "dark" ? "dark" :"light"} />
         </div>
         
     )
