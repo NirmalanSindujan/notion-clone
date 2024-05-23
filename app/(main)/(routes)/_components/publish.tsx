e0a15a8bd8a0c2789009e816034b5418b06db0ca
sindujan <sindujan96@gmail.com>
2024-05-23 12:17:21 +0530

    Public view

--- app/(main)/(routes)/_components/publish.tsx
+++ app/(main)/(routes)/_components/publish.tsx
@@ -12,7 +12,7 @@ import { api } from "@/convex/_generated/api";
 import { useState } from "react";
 import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
-import { Globe } from "lucide-react";
+import { Check, Copy, Globe } from "lucide-react";
 
 interface PublishProps {
   initialData: Doc<"documents">;
@@ -51,7 +51,7 @@ export const Publish = ({ initialData }: PublishProps) => {
       id: initialData._id,
       isPublished: false,
     }).finally(() => {
-      setIsSubmitting(true);
+      setIsSubmitting(false);
     });
 
     toast.promise(promise, {
@@ -82,7 +82,41 @@ export const Publish = ({ initialData }: PublishProps) => {
 
       <PopoverContent className="w-72" align="end" alignOffset={8} forceMount>
         {initialData.isPublished ? (
-          <div className="space-y-4">Publis</div>
+          <div className="space-y-4">
+            <div className=" flex items-center gap-x-2">
+              <Globe className="text-sky-500 animate-pulse h-4 w-4" />
+              <p className="text-xs font-medium text-sky-500">
+                This note is live
+              </p>
+            </div>
+            <div className="flex items-center">
+              
+              <input 
+              className="flex-1 px-2 text-xs border rounded-l-md h-8 bg-muted truncate"
+              value={url} 
+              disabled
+              />
+
+              <Button
+              onClick={onCopy}
+              disabled={copied}
+              className=" h-8 rounded-l-none"
+              >
+                {copied ? <Check className="h-4 w-4"/> :(
+                  <Copy className="h-4 w-4"/>
+                )}
+              </Button>
+            </div>
+
+            <Button
+               size={'sm'}
+               className="w-full text-xs"
+               disabled={isSubmitting}
+               onClick={onUnPublish}
+               >
+              UnPublich
+            </Button>
+          </div>
         ) : (
           <div className="flex flex-col items-center justify-center">
             <Globe className="h-8 w-8 text-muted-foreground mb-2" />
@@ -90,12 +124,14 @@ export const Publish = ({ initialData }: PublishProps) => {
             <span className="text-xs text-muted-foreground mb-4">
                 Share Your Work with others
             </span>
-            <Button disabled={isSubmitting}
+            <Button
+             disabled={isSubmitting}
             onClick={onPublish}
             className="w-full text-xs"
             size="sm"
             >
-                Publish</Button>
+                Publish
+                </Button>
           </div>
         )}
       </PopoverContent>
