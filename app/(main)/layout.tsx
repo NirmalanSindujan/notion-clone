882a0934f24d61d7422e88f75756ea5e4bafa36f
Sindujan <37435563+NirmalanSindujan@users.noreply.github.com>
2025-03-16 22:22:27 +0530

    test

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -25,6 +25,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
   <div className="h-full flex dark:bg-[#1F1F1F]">
     <Navigation />
+    //test
     <main className="flex-1 h-full overflow-y-auto">
       <SearchCommand />
     {children}
